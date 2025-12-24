32df278905a01566882f666c38a11885ef1043e9
priyanshuxoxo <srivastavapriyanshu201@gmail.com>
2025-12-24 17:14:09 +0530

    fix:cors policy and added frontend url

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
-import path from "path";
 import cookieParser from "cookie-parser";
-import { fileURLToPath } from "url";
 
 import { connectDB } from "./db/connectDB.js";
 import authRoutes from "./routes/auth.routes.js";
@@ -13,31 +11,35 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-const __dirname = path.resolve();
 const allowedOrigins = [
+  "https://auth-tutorials.vercel.app",
   "http://localhost:5173",
-  "https://auth-tutorials.onrender.com",
 ];
-app.use(
-  cors({
-    origin: function (origin, callback) {
-      if (!origin) return callback(null, true);
 
-      if (allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
-    credentials: true,
-  })
-);
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (!origin) return callback(null, true);
+
+    if (allowedOrigins.includes(origin)) {
+      return callback(null, true);
+    } else {
+      return callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+};
+
+app.use(cors(corsOptions));
+app.options("*", cors(corsOptions));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
+
 app.use((req, res) => {
   res.status(404).json({
     success: false,
